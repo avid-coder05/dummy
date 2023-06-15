f355b334d19fbd365b317274fa6148739287c99e
avid-coder05 <tandiv15.bansal@gmail.com>
2023-06-15 12:57:43 +0530

    Initial device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_xaga.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_xaga-user \
+    aosp_xaga-userdebug \
+    aosp_xaga-eng
---
+++ aosp_xaga.mk
@@ -0,0 +1,28 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from xaga device
+$(call inherit-product, device/xiaomi/xaga/device.mk)
+
+PRODUCT_DEVICE := xaga
+PRODUCT_NAME := aosp_xaga
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note11T Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2460
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_SCREEN_DENSITY := 440
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_SUPPORTS_QUICK_TAP := true
